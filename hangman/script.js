4a451d06fca7eadfc028da326748fd1cb9a8cc42
Yar Night <yaroslav239@gmail.com>
2024-01-05 21:01:15 +0300

    feat: add questions

--- hangman/script.js
+++ hangman/script.js
@@ -1,3 +1,14 @@
 window.addEventListener("keyup", (e) => {
     console.log(e);
 })
+
+import {questions} from './questions.js';
+
+console.log(questions.length)
+
+function f() {
+    const inputs = [...document.querySelectorAll(".input")];
+    const word = Object.keys(questions[1])[0].split("");
+    console.log(word);
+    inputs.forEach((item, index) => item.textContent = word[index])
+}
